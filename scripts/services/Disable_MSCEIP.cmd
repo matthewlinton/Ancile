4b94317a5d940bb18a08b27b069e55cf76aa2453
Matthew Linton <matthew.linton@gmail.com>
2016-11-10 02:59:30 -0500

    Added code to remove skyDrive explorer icon Added spynettest.microsoft.com to domain names

--- scripts/services/Disable_MSCEIP.cmd
+++ scripts/services/Disable_MSCEIP.cmd
@@ -1,5 +1,6 @@
 @REM Disable Microsoft Customer Experience Improvement Program
 ECHO Disabling Microsoft Customer Experience Improvement Program: >> "%LOGFILE%"
 ECHO ** Customer Experience Improvement Program
+
 SET rkey=HKEY_LOCAL_MACHINE\SOFTWARE\microsoft\sqmclient\windows
 reg ADD "%rkey%" /f /t reg_dword /v ceipenable /d 0 >> "%LOGFILE%" 2>&1
